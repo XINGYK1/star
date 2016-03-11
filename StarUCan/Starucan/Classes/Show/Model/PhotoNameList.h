d34c186d7a6649dc52c6acb533e311e2eb8c548e
FTL_2016 <18800035956@163.com>
2016-03-11 12:07:54 +0800

    change

---
+++ StarUCan/Starucan/Classes/Show/Model/PhotoNameList.h
@@ -0,0 +1,13 @@
+//
+//  PhotoNameList.h
+//  Starucan
+//
+//  Created by vgool on 16/3/11.
+//  Copyright © 2016年 vgool. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PhotoNameList : NSMutableArray
++(instancetype)shareInstance;
+@end
